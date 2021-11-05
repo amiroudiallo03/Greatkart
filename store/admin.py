a6305cb9ce82a8cdf1a58a17d58427dff2c76fbc
amiroudiallo03 <amiroudiallo03@gmail.com>
2021-11-05 17:46:30 +0000

    creation models category et product

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from . import models
+from django.utils.safestring import mark_safe
 # Register your models here.
+
+@admin.register(models.Category)
+class CategoriyAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'picture', 'description')
+    search_field = ('category_name',)
+    date_hierarchy = 'date_add'
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'image_view', 'description', 'price', 'category', 'status')
+    search_field = ('product_name',)
+    date_hierarchy = 'date_add'
+
+    def image_view(self, obj):
+
+        return mark_safe(f'<img src="{obj.images.url}" style="width:100px; height:100px">')
--- store/models.py
+++ store/models.py
@@ -1,3 +1,44 @@
 from django.db import models
-
+from autoslug import AutoSlugField
 # Create your models here.
+
+
+class Base(models.Model):
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+    class Meta:
+        abstract=True
+
+  
+class Category(Base):
+    category_name = models.CharField(max_length=128)
+    slug = AutoSlugField(populate_from='category_name')
+    picture = models.FileField(upload_to="picture_categorie")
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
+    
+class Product(Base):
+    product_name = models.CharField(max_length=128)
+    slug = AutoSlugField(populate_from= 'product_name')
+    images = models.FileField(upload_to='images_product')
+    price = models.FloatField()
+    description = models.TextField(blank=True)
+    stock = models.IntegerField()
+    is_availaible = models.BooleanField(default=True)
+    category = models.ForeignKey("Category", related_name='category_product', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.product_name
+    
+        
